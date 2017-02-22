3e8a736e78199e8a9219259ea8956c5d3d581e8a
dz14 <duy.le14@live.com>
2017-02-22 17:23:24 -0500

    reorganized directories for view-engine

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var port = process.env.PORT||3000;
 var express = require('express');
+var path    = require("path");
+
 
 //the server code
 //create the server and also handle requests
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 3000));
 //start the server and notify it start with the address in the console
 app.listen(port, function () {
   console.log('Server running on port', app.get('port'));
 });
 
 app.get('/',function(req,res){
-     res.sendFile('index.html');
+    res.render('index');
 });
