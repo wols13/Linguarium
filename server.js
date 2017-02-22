b89d162e5b599071c7499f0f35f24ee506c770b4
dz14 <duy.le14@live.com>
2017-02-22 16:18:54 -0500

    added package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+var port = 3000;
+var express = require('express');
+
+//the server code
+//create the server and also handle requests
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+//start the server and notify it start with the address in the console
+app.listen(port, function () {
+  console.log('Server running on port', app.get('port'));
+});
