0e9a3ac33d19974bb69de848b0d3eaa825222b23
dz14 <duy.le14@live.com>
2017-02-23 20:27:50 -0500

    added logs for coordinates

--- public/js/tools/visualcue.js
+++ public/js/tools/visualcue.js
@@ -2,6 +2,7 @@ var socket = io.connect(window.location.hostname);
 
 // Handling incoming text message
 socket.on('coordinates', function(data){
+	console.log(data);
 	// $("#teacher-display").
 	var color = '#ff0000';
   var size = '10px';
