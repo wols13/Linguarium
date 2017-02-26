974fefe5c754236887786a9053d9c4b74c764f7b
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2017-02-26 13:21:54 -0500

    Revert "updated chat.js"
    This reverts commit 1843aa0368a8f90e356acf6c315cdd1702654e24.

--- public/js/tools/chat.js
+++ public/js/tools/chat.js
@@ -22,7 +22,7 @@ $("#new-message").keypress(function(e){
     if (keyCode == '13'){
 		// Send the text, clear input field and update message buble
 		var new_message = $("#new-message").val();
-		socket.broadcast.emit('text_message', new_message);
+		socket.emit('text_message', new_message);
 
 		$("#new-message").val('');
 		new_message = "<span class='my_message'>" + name + ": " + new_message + "</span><br>";
