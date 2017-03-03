52c6346c7240b16baaa50aab149ba910d50eaaf1
Ayodeji Oyewole <ayodeji.oyewole@gmail.com>
2017-03-03 15:28:36 -0500

    First attempt to fix reload issue

--- public/js/tools/script_control.js
+++ public/js/tools/script_control.js
@@ -1,5 +1,5 @@
 $.getScript("js/tools/socket.js");
-$.getScript("js/tools/chat.js");
-$.getScript("js/tools/user_list.js");
-$.getScript("js/tools/visualcue.js");
-$.getScript("js/tools/tab_control.js");
+//$.getScript("js/tools/chat.js");
+//$.getScript("js/tools/user_list.js");
+//$.getScript("js/tools/visualcue.js");
+//$.getScript("js/tools/tab_control.js");
