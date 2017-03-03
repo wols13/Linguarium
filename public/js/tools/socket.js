9ba56fd8f7569465c5f769f0e440fb4699a23289
dz14 <duy.le14@live.com>
2017-03-03 17:44:10 -0500

    commit 1
    moved visual cue code to socket.js

--- public/js/tools/socket.js
+++ public/js/tools/socket.js
@@ -55,6 +55,55 @@ window.onload = function(){
 		}
 	});
 
+	// Handling incoming text message
+socket.on('coordinates', function(data){
+	console.log(data);
+	// $("#teacher-display").
+	var color = '#ff0000';
+  var size = '10px';
+  // $("#teacher-display").append(
+  // $('<div id="visualcue_canvas"></div>')
+  //   .css('position', 'absolute')
+  //   .css('top', data[1] - 55 + 'px')
+  //   .css('left', data[0] - 5 + 'px')
+  //   .css('width', size)
+  //   .css('height', size)
+  //   .css('background-color', color)
+  //       );
+	  draw_dot(data[1], data[0] );
+		setTimeout(remove_dot, 1500);
+});
+
+// Handling event when user clicks enter to send text message
+$("#workspace-main").click(function(){
+		var coor = [];
+		var x = event.clientX;
+		var y = event.clientY;
+		coor.push(x);
+		coor.push(y);
+		// var coor = "X coords: " + x + ", Y coords: " + y;
+		socket.emit('coordinates', coor);
+});
+
+function draw_dot(coor1,coor0){
+	var color = '#ff0000';
+  var size = '10px';
+	$("#workspace-main").append(
+	$('<div id="visualcue_canvas"></div>')
+		.css('position', 'absolute')
+		.css('top', coor1 - 60 + 'px')
+		.css('left', coor0 - 310 + 'px')
+		.css('width', size)
+		.css('height', size)
+		.css('background-color', color)
+				);
+}
+
+function remove_dot(){
+	$('#visualcue_canvas').remove();
+}
+
+
 	//User disconnected, send signal for disconnect
 	$(window).on("beforeunload", function() {
 		socket.emit('user_disconnected', {user: name});
