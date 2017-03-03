3a7022ff932fb964dec15db02b347b6fee49fcef
dz14 <duy.le14@live.com>
2017-03-02 20:43:43 -0500

    added visual cue animation

--- public/js/tools/visualcue.js
+++ public/js/tools/visualcue.js
@@ -4,15 +4,17 @@ socket.on('coordinates', function(data){
 	// $("#teacher-display").
 	var color = '#ff0000';
   var size = '10px';
-  $("#teacher-display").append(
-  $('<div></div>')
-    .css('position', 'absolute')
-    .css('top', data[1] - 55 + 'px')
-    .css('left', data[0] - 5 + 'px')
-    .css('width', size)
-    .css('height', size)
-    .css('background-color', color)
-        );
+  // $("#teacher-display").append(
+  // $('<div id="visualcue_canvas"></div>')
+  //   .css('position', 'absolute')
+  //   .css('top', data[1] - 55 + 'px')
+  //   .css('left', data[0] - 5 + 'px')
+  //   .css('width', size)
+  //   .css('height', size)
+  //   .css('background-color', color)
+  //       );
+	  draw_dot(data[1], data[0] );
+		setTimeout(remove_dot, 1500);
 });
 
 // Handling event when user clicks enter to send text message
@@ -25,3 +27,21 @@ $("#teacher-display").click(function(){
 		// var coor = "X coords: " + x + ", Y coords: " + y;
 		socket.emit('coordinates', coor);
 });
+
+function draw_dot(coor1,coor0){
+	var color = '#ff0000';
+  var size = '10px';
+	$("#teacher-display").append(
+	$('<div id="visualcue_canvas"></div>')
+		.css('position', 'absolute')
+		.css('top', coor1 - 55 + 'px')
+		.css('left', coor0 - 5 + 'px')
+		.css('width', size)
+		.css('height', size)
+		.css('background-color', color)
+				);
+}
+
+function remove_dot(){
+	$('#visualcue_canvas').remove();
+}
